9e2cb9c3ce93ead83a4aa9f7b8fe81f5bd87ea99
Arafat-Zaman <arafat.zaman.anik@gmail.com>
2020-10-10 19:55:30 +0600

    Edit Modification

--- ControllerOfficeStudentResultSystem/Controllers/HomeController.cs
+++ ControllerOfficeStudentResultSystem/Controllers/HomeController.cs
@@ -23,8 +23,9 @@ namespace ControllerOfficeStudentResultSystem.Controllers
 			this.hostingEnvironment = hostingEnvironment;
 		}
 
-		public ViewResult Index()
-		{
+		public ViewResult Index(string searchBy, string search)
+		{		
+
 			var model = _studentRepository.GetAllStudents();
 			return View(model);
 		}
@@ -42,21 +43,72 @@ namespace ControllerOfficeStudentResultSystem.Controllers
 		[HttpGet]
 		public ViewResult Create()
 		{
+			
 			return View();
 		}
+		[HttpGet]
+		public ViewResult Edit(int ID)
+		{
+			Student student = _studentRepository.GetStudent(ID);
+			StudentEditViewModel studentEditViewModel = new StudentEditViewModel()
+			{
+				ID = student.ID,
+				SubjectName = student.SubjectName,
+				Year = student.Year,
+				Program = student.Program,
+				ExistingExcelPath = student.ExcelPath
+
+			};
+			return View(studentEditViewModel);
+		}
+		[HttpPost]
+		public IActionResult Edit(StudentEditViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				Student student = _studentRepository.GetStudent(model.ID);
+				student.SubjectName = model.SubjectName;
+				student.Year = model.Year;
+				student.Program = model.Program;
+				
+				if(model.ExcelFile != null)
+				{
+					if(model.ExistingExcelPath != null)
+					{
+						string filePath = Path.Combine(hostingEnvironment.WebRootPath, "images", model.ExistingExcelPath);
+						System.IO.File.Delete(filePath);
+					}
+					student.ExcelPath = ProcessUploadFile(model);
+				}
+				_studentRepository.Update(student);
+				return RedirectToAction("index");
+			}
+			return View();
+		}
+
+		private string ProcessUploadFile(StudentCreateViewModel model)
+		{
+			string uniqueFileName = null;
+			if (model.ExcelFile != null)
+			{
+				string uploadFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+				uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ExcelFile.FileName;
+				string filePath = Path.Combine(uploadFolder, uniqueFileName);
+				using (var fileStream = new FileStream(filePath, FileMode.Create))
+				{
+					model.ExcelFile.CopyTo(fileStream);
+				}
+			}
+
+			return uniqueFileName;
+		}
+
 		[HttpPost]
 		public IActionResult Create(StudentCreateViewModel model)
 		{
 			if (ModelState.IsValid)
 			{
-				string uniqueFileName = null;
-				if(model.ExcelFile != null)
-				{
-					string uploadFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
-					uniqueFileName = Guid.NewGuid().ToString() + "_" + model.ExcelFile.FileName;
-					string filePath = Path.Combine(uploadFolder, uniqueFileName);
-					model.ExcelFile.CopyTo(new FileStream(filePath, FileMode.Create));
-				}
+				string uniqueFileName = ProcessUploadFile(model);
 
 
 				Student newStudent = new Student
@@ -73,5 +125,7 @@ namespace ControllerOfficeStudentResultSystem.Controllers
 			}
 			return View();
 		}
+
+
 	}
 }
---
+++ ControllerOfficeStudentResultSystem/ViewModels/StudentEditViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllerOfficeStudentResultSystem.ViewModels
+{
+	public class StudentEditViewModel : StudentCreateViewModel
+	{
+		public int ID { get; set; }
+		public string ExistingExcelPath { get; set; }
+	}
+}
