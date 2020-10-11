c565fb2fbf057d43df17bd44c19122b687b24207
Arafat-Zaman <arafat.zaman.anik@gmail.com>
2020-10-12 01:21:56 +0600

    Upload download search complete

--- ControllerOfficeStudentResultSystem/Controllers/HomeController.cs
+++ ControllerOfficeStudentResultSystem/Controllers/HomeController.cs
@@ -126,6 +126,49 @@ namespace ControllerOfficeStudentResultSystem.Controllers
 			return View();
 		}
 
+		public async Task<IActionResult> Download(string filename)
+		{
+			if (filename == null)
+				return Content("filename not present");
+
+			var path = Path.Combine(
+						   Directory.GetCurrentDirectory(),
+						   "wwwroot/images", filename);
+
+			var memory = new MemoryStream();
+			using (var stream = new FileStream(path, FileMode.Open))
+			{
+				await stream.CopyToAsync(memory);
+			}
+			memory.Position = 0;
+			return File(memory, GetContentType(path), Path.GetFileName(path));
+		}
+
+		private string GetContentType(string path)
+		{
+			var types = GetMimeTypes();
+			var ext = Path.GetExtension(path).ToLowerInvariant();
+			return types[ext];
+		}
+
+		private Dictionary<string, string> GetMimeTypes()
+		{
+			return new Dictionary<string, string>
+			{
+				{".txt", "text/plain"},
+				{".pdf", "application/pdf"},
+				{".doc", "application/vnd.ms-word"},
+				{".docx", "application/vnd.ms-word"},
+				{".xls", "application/vnd.ms-excel"},
+				{".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+				{".png", "image/png"},
+				{".jpg", "image/jpeg"},
+				{".jpeg", "image/jpeg"},
+				{".gif", "image/gif"},
+				{".csv", "text/csv"}
+			};
+		}
+
 
 	}
 }
