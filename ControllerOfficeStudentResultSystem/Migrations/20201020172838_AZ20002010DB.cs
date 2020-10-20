ff3f330ccb49f0787f14afb9b4d57c6b6986c0fe
Arafat-Zaman <arafat.zaman.anik@gmail.com>
2020-10-21 00:08:18 +0600

    Cosmetic changes: Cancel, Back, List button.

---
+++ ControllerOfficeStudentResultSystem/Migrations/20201020172838_AZ20002010DB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ControllerOfficeStudentResultSystem.Migrations
+{
+    public partial class AZ20002010DB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
